56931b28332b4dc5d29e515011cf8bc5934fe11d
Arturo Navarro <arturonavarro@outlook.com>
2021-06-07 23:18:44 -0500

    Adding unit testing

--- src/components/navigation/router/Router.test.js
+++ src/components/navigation/router/Router.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { themeProviderWrapper } from 'utils/test';
 import Router from './Router';
 
+jest.mock('components/common/header', () => () => <div>Bux Header</div>);
 jest.mock('components/pages/home', () => () => <div>Home Page</div>);
 jest.mock('components/pages/company', () => () => <div>Company Page</div>);
 
@@ -21,21 +22,21 @@ describe('<Router />', () => {
   it('should render correctly', () => {
     setup();
 
-    expect(screen.getByText('bux-logo')).toBeInTheDocument();
+    expect(screen.getByText('Bux Header')).toBeInTheDocument();
     expect(screen.getByText('Home Page')).toBeInTheDocument();
   });
 
   it('should render the company page', () => {
     setup({ initialRoute: '/company/appl' });
 
-    expect(screen.getByText('bux-logo')).toBeInTheDocument();
+    expect(screen.getByText('Bux Header')).toBeInTheDocument();
     expect(screen.getByText('Company Page')).toBeInTheDocument();
   });
 
   it('should render the 404 page', () => {
     setup({ initialRoute: '/not-a-good-route' });
 
-    expect(screen.getByTestId('bux-logo')).toBeInTheDocument();
+    expect(screen.getByText('Bux Header')).toBeInTheDocument();
     expect(screen.getByText('404 Not found')).toBeInTheDocument();
   });
 });
